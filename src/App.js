2b3b9c774795f3bcb2323ae8f1ab0319b3fe485f
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-06-06 17:57:07 -0400

    Add front end template containing bootstrap jumbotron with buttons (no functionality)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container justify-content-center">
+          <div className="jumbotron">
+            <div className="row">
+              <div className="col">
+                <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                  <div className="btn-group mr-2" role="group" aria-label="First group">
+                    <button type="button" className="btn btn-primary" onClick={this.addRow}>Add Row</button>
+                    <button type="button" className="btn btn-primary" onClick={this.addColumn}>Add Column</button>
+                  </div>
+                  <div className="btn-group mr-2" role="group" aria-label="Second group">
+                    <button type="button" className="btn btn-primary" onClick={this.removeRow}>Remove Row</button>
+                    <button type="button" className="btn btn-primary" onClick={this.removeColumn}>Remove Column</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col">  
+                <label htmlFor="colors">Choose a color:</label>
+
+                <select name="colors" className="colors" value={this.state.cellColor} onChange={this.chooseColor}>
+                  <option value="#FFFFFF">Color</option>
+                  <option value="#FF0000">Red</option>
+                  <option value="#00FF00">Green</option>
+                  <option value="#0000FF">Blue</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {/* Insert Table */}
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
